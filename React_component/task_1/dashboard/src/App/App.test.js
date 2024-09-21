20420ae803d780e2701134f72a0400095369ccc5
ChernetAsmamaw <c.asmamaw@alustudent.com>
2024-09-21 11:56:06 +0200

    app.js test update

--- React_component/task_1/dashboard/src/App/App.test.js
+++ React_component/task_1/dashboard/src/App/App.test.js
@@ -9,15 +9,4 @@ describe("<App />", () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.exists()).toEqual(true);
 	});
-
-	it('Verify if CourseList is displayed when isLoggedIn is false', () => {
-		const wrapper = shallow(<App />);
-		expect(wrapper.find(CourseList)).toHaveLength(0);
-	});
-
-	it('Verify if CourseList is displayed when isLoggedIn is true', () => {
-		const wrapper = shallow(<App isLoggedIn={true} />);
-		expect(wrapper.find(CourseList)).toHaveLength(1);
-		expect(wrapper.find(Login)).toHaveLength(0);
-	});
 });
