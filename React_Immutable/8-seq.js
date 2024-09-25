6ae2a485c9d584162bb501056a5087a19f5cd178
ChernetAsmamaw <c.asmamaw@alustudent.com>
2024-09-25 19:44:48 +0200

    update to seq

--- React_Immutable/8-seq.js
+++ React_Immutable/8-seq.js
@@ -6,13 +6,13 @@ const { Seq } = immutable;
 // and console.log first and last name with first letter in capital
 export default function printBestStudents(grades) {
   // convert the object to a Seq and filter the students with a grade >= 70
-  const students = Seq(grades).filter(student => student.score >= 70)
-    .map(student => (
-        {
+  const students = Seq(grades).filter((student) => student.score >= 70)
+    .map((student) => (
+      {
         ...student,
         firstName: `${student.firstName.charAt(0).toUpperCase()}${student.firstName.slice(1)}`,
-        lastName: `${student.lastName.charAt(0).toUpperCase()}${student.lastName.slice(1)}`
-        }
+        lastName: `${student.lastName.charAt(0).toUpperCase()}${student.lastName.slice(1)}`,
+      }
     ));
 
   // Log filtered students
